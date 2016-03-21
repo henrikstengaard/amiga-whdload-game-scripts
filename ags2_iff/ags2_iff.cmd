0889ef2819a10407bf4029906870339a55d0ad36
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2016-03-21 09:28:23 +0100

    Updated AGS2 iff batch command

--- ags2_iff/ags2_iff.cmd
+++ ags2_iff/ags2_iff.cmd
@@ -1,7 +1,7 @@
 ::AGA
-imgtoiff-background-aga.py AGA.pal AGA-Background.png AGA-Background.iff
-imgtoiff-background-aga.py AGA.pal AGA-Empty.png AGA-Empty.iff
+imgtoiff-background-aga.py --aga --pack 1 AGA.pal AGA-Background.png AGA-Background.iff
+imgtoiff-background-aga.py --aga --pack 1 AGA.pal AGA-Empty.png AGA-Empty.iff
 
 ::OCS
-imgtoiff-background-ocs.py --ocs OCS.pal OCS-Background.png OCS-Background.iff
-imgtoiff-background-ocs.py --ocs OCS.pal OCS-Empty.png OCS-Empty.iff
+imgtoiff-background-ocs.py --ocs --pack 1 OCS.pal OCS-Background.png OCS-Background.iff
+imgtoiff-background-ocs.py --ocs --pack 1 OCS.pal OCS-Empty.png OCS-Empty.iff
