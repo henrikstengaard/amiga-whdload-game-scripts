407dc4e51507e5a44fd3ec492f52834dc7c329c3
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2016-04-13 11:04:35 +0200

    Updated ags2_iff batch command to only use ImageToIff script to build AGS2 menu images

--- ags2_iff/ags2_iff.cmd
+++ ags2_iff/ags2_iff.cmd
@@ -1,13 +1,13 @@
 ::AGA
-imgtoiff-background-aga.py --aga --pack 1 AGA.pal AGA-Background.png AGA-Background.iff
-imgtoiff-background-aga.py --aga --pack 1 AGA.pal AGA-Empty.png AGA-Empty.iff
+::imgtoiff-background-aga.py --aga --pack 1 AGA.pal AGA-Background.png AGA-Background.iff
+::imgtoiff-background-aga.py --aga --pack 1 AGA.pal AGA-Empty.png AGA-Empty.iff
 
-powershell -ExecutionPolicy Bypass -File ImageToIff.ps1 -imagePath AGA-Background.png -iffPath AGA-Background2.iff
-powershell -ExecutionPolicy Bypass -File ImageToIff.ps1 -imagePath AGA-Empty.png -iffPath AGA-Empty2.iff
+powershell -ExecutionPolicy Bypass -File ImageToIff.ps1 -imagePath AGA-Background.png -iffPath AGA-Background.iff
+powershell -ExecutionPolicy Bypass -File ImageToIff.ps1 -imagePath AGA-Empty.png -iffPath AGA-Empty.iff
 
 ::OCS
-imgtoiff-background-ocs.py --ocs --pack 1 OCS.pal OCS-Background.png OCS-Background.iff
-imgtoiff-background-ocs.py --ocs --pack 1 OCS.pal OCS-Empty.png OCS-Empty.iff
+::imgtoiff-background-ocs.py --ocs --pack 1 OCS.pal OCS-Background.png OCS-Background.iff
+::imgtoiff-background-ocs.py --ocs --pack 1 OCS.pal OCS-Empty.png OCS-Empty.iff
 
-powershell -ExecutionPolicy Bypass -File ImageToIff.ps1 -imagePath OCS-Background.png -iffPath OCS-Background2.iff
-powershell -ExecutionPolicy Bypass -File ImageToIff.ps1 -imagePath OCS-Empty.png -iffPath OCS-Empty2.iff
+powershell -ExecutionPolicy Bypass -File ImageToIff.ps1 -imagePath OCS-Background.png -iffPath OCS-Background.iff
+powershell -ExecutionPolicy Bypass -File ImageToIff.ps1 -imagePath OCS-Empty.png -iffPath OCS-Empty.iff
