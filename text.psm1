03166daab51d0f535faed540c827196f8b5d64d1
Henrik Stengaard <hst@firstrealize.com>
2017-12-19 14:41:31 +0100

    Added text module

---
+++ text.psm1
@@ -0,0 +1,38 @@
+function RemoveDiacritics([string]$text)
+{
+	$textFormD = $text.Normalize([System.Text.NormalizationForm]::FormD).ToCharArray()
+	$sb = New-Object -TypeName "System.Text.StringBuilder"
+
+	ForEach ($c in $textFormD)
+	{
+		$uc = [System.Globalization.CharUnicodeInfo]::GetUnicodeCategory($c)
+		if ($uc -ne [System.Globalization.UnicodeCategory]::NonSpacingMark)
+		{
+			[void]$sb.Append($c)
+		}
+	}
+	
+	return $sb.ToString().Normalize([System.Text.NormalizationForm]::FormC)
+}
+
+function ConvertSuperscript([string]$text)
+{
+	$textFormKd = $text.Normalize([System.Text.NormalizationForm]::FormKD).ToCharArray()
+	$sb = New-Object -TypeName "System.Text.StringBuilder"
+
+	ForEach ($c in $textFormKd)
+	{
+		$uc = [System.Globalization.CharUnicodeInfo]::GetUnicodeCategory($c)
+		if ($uc -ne [System.Globalization.UnicodeCategory]::NonSpacingMark)
+		{
+			[void]$sb.Append($c)
+		}
+	}
+
+	return $sb.ToString().Normalize([System.Text.NormalizationForm]::FormKC)
+}
+
+function NormalizeText([string]$text)
+{
+	return RemoveDiacritics (ConvertSuperscript $text)
+}
