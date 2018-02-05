e9f9eb7870d0313d6c35182b860ea9f347f4de75
Henrik Stengaard <hst@firstrealize.com>
2018-02-05 09:50:51 +0100

    Updated remove entries script

--- build_whdload_remove_script.ps1
+++ build_whdload_remove_script.ps1
@@ -1,10 +1,10 @@
-# Build WHDLoad Remove Script
+# Build entries remove script
 # ---------------------------
 #
 # Author: Henrik Nørfjand Stengaard
-# Date:   2017-10-14
+# Date:   2018-01-16
 #
-# A PowerShell script to build whdload remove script used to remove unwanted language or hardware versions of whdload demos or games.
+# A PowerShell script to build entries remove script used to remove unwanted language or hardware versions of demos or games.
 
 
 Param(
@@ -51,7 +51,7 @@ $entries = @()
 foreach($entriesFile in ($entriesFiles -Split ','))
 {
 	$entriesFile = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($entriesFile)
-	Write-Host ("Reading whdload slaves file '{0}'" -f $entriesFile)
+	Write-Host ("Reading entries file '{0}'" -f $entriesFile)
 	$entries += import-csv -delimiter ';' -path $entriesFile -encoding utf8 
 }
 
@@ -61,7 +61,7 @@ $languagesIndex = @{}
 $hardwaresIndex = @{}
 
 
-# process whdload slave items
+# process entries
 foreach($entry in $entries)
 {
 	$languages = @()
@@ -119,61 +119,61 @@ foreach($entry in $entries)
 }
 
 
-$removeWhdloadsLines = @()
-$removeWhdloadsLines += "; Remove WHDLoads"
-$removeWhdloadsLines += "; ---------------"
-$removeWhdloadsLines += "; Author: Henrik Noerfjand Stengaard"
-$removeWhdloadsLines += ("; Date: {0}" -f (Get-Date -format "yyyy-MM-dd"))
-$removeWhdloadsLines += ";"
-$removeWhdloadsLines += "; A remove WHDLoads script to remove unwanted WHDLoads language and hardware versions."
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += ("; Show error, if WHDLoads assign '{0}' doesn't exist" -f $assignName)
-$removeWhdloadsLines += ("Assign >NIL: EXISTS {0}:" -f $assignName)
-$removeWhdloadsLines += "IF WARN"
-$removeWhdloadsLines += ("  REQUESTCHOICE >NIL: ""Error"" ""WHDLoads assign '{0}' doesn't exist!*N*NPlease verify WHDLoads assign is configured."" ""OK""" -f $assignName)
-$removeWhdloadsLines += "  SKIP end"
-$removeWhdloadsLines += "ENDIF"
+$removeEntriesLines = @()
+$removeEntriesLines += "; Remove entries"
+$removeEntriesLines += "; --------------"
+$removeEntriesLines += "; Author: Henrik Noerfjand Stengaard"
+$removeEntriesLines += ("; Date: {0}" -f (Get-Date -format "yyyy-MM-dd"))
+$removeEntriesLines += ";"
+$removeEntriesLines += "; An AmigaDOS script to remove unwanted entries language and hardware versions."
+$removeEntriesLines += ''
+$removeEntriesLines += ''
+$removeEntriesLines += ("; Show error, if assign '{0}' doesn't exist" -f $assignName)
+$removeEntriesLines += ("Assign >NIL: EXISTS {0}:" -f $assignName)
+$removeEntriesLines += "IF WARN"
+$removeEntriesLines += ("  REQUESTCHOICE >NIL: ""Error"" ""Assign '{0}' doesn't exist!*N*NPlease verify assign is configured."" ""OK""" -f $assignName)
+$removeEntriesLines += "  SKIP end"
+$removeEntriesLines += "ENDIF"
 
-$removeWhdloadsMenuLines = @()
-$removeWhdloadsOptionLines = @()
-$removeWhdloadsRunLines = @()
-$removeWhdloadsTempLines = @()
+$removeEntriesMenuLines = @()
+$removeEntriesOptionLines = @()
+$removeEntriesRunLines = @()
+$removeEntriesTempLines = @()
 
 foreach($language in ($languagesIndex.Keys | Sort-Object))
 {
 	$languageId = CalculateMd5FromText $language.ToUpper()
 
-	$removeWhdloadsMenuLines += ""
-	$removeWhdloadsMenuLines += ("; Language '{0}' menu" -f $language.ToUpper())
-	$removeWhdloadsMenuLines += ("echo ""Language {0} : "" NOLINE >>T:removewhdloadsmenu" -f $language.ToUpper())
-	$removeWhdloadsMenuLines += ("IF EXISTS ""T:{0}""" -f $languageId)
-	$removeWhdloadsMenuLines += "  echo ""YES"" >>T:removewhdloadsmenu"
-	$removeWhdloadsMenuLines += "ELSE"
-	$removeWhdloadsMenuLines += "  echo ""NO "" >>T:removewhdloadsmenu"
-	$removeWhdloadsMenuLines += "ENDIF"
+	$removeEntriesMenuLines += ""
+	$removeEntriesMenuLines += ("; Language '{0}' menu" -f $language.ToUpper())
+	$removeEntriesMenuLines += ("echo ""Language {0} : "" NOLINE >>T:removeentriesmenu" -f $language.ToUpper())
+	$removeEntriesMenuLines += ("IF EXISTS ""T:{0}""" -f $languageId)
+	$removeEntriesMenuLines += "  echo ""YES"" >>T:removeentriesmenu"
+	$removeEntriesMenuLines += "ELSE"
+	$removeEntriesMenuLines += "  echo ""NO "" >>T:removeentriesmenu"
+	$removeEntriesMenuLines += "ENDIF"
 
-	$removeWhdloadsMenuIndex++
-	$removeWhdloadsOptionLines += ""
-	$removeWhdloadsOptionLines += ("; Language '{0}' option" -f $language.ToUpper())
-	$removeWhdloadsOptionLines += ("IF ""`$removewhdloadsmenu"" eq ""{0}""" -f $removeWhdloadsMenuIndex)
-	$removeWhdloadsOptionLines += ("  IF EXISTS ""T:{0}""" -f $languageId)
-	$removeWhdloadsOptionLines += ("    Delete >NIL: ""T:{0}""" -f $languageId)
-	$removeWhdloadsOptionLines += "  ELSE"
-	$removeWhdloadsOptionLines += ("    echo """" NOLINE >""T:{0}""" -f $languageId)
-	$removeWhdloadsOptionLines += "  ENDIF"
-	$removeWhdloadsOptionLines += "ENDIF"
+	$removeEntriesMenuIndex++
+	$removeEntriesOptionLines += ""
+	$removeEntriesOptionLines += ("; Language '{0}' option" -f $language.ToUpper())
+	$removeEntriesOptionLines += ("IF ""`$removeentriesmenu"" eq ""{0}""" -f $removeEntriesMenuIndex)
+	$removeEntriesOptionLines += ("  IF EXISTS ""T:{0}""" -f $languageId)
+	$removeEntriesOptionLines += ("    Delete >NIL: ""T:{0}""" -f $languageId)
+	$removeEntriesOptionLines += "  ELSE"
+	$removeEntriesOptionLines += ("    echo """" NOLINE >""T:{0}""" -f $languageId)
+	$removeEntriesOptionLines += "  ENDIF"
+	$removeEntriesOptionLines += "ENDIF"
 	
-	$removeWhdloadsRunLines += ""
-	$removeWhdloadsRunLines += ("; Remove language '{0}', if it's selected" -f $language.ToUpper())
-	$removeWhdloadsRunLines += ("IF EXISTS ""T:{0}""" -f $languageId)
-	$removeWhdloadsRunLines += ("  echo ""Language {0}...""" -f $language.ToUpper())
-	$removeWhdloadsRunLines += ("  execute ""Remove-Language-{0}""" -f $language.ToUpper())
-	$removeWhdloadsRunLines += "ENDIF"
+	$removeEntriesRunLines += ""
+	$removeEntriesRunLines += ("; Remove language '{0}', if it's selected" -f $language.ToUpper())
+	$removeEntriesRunLines += ("IF EXISTS ""T:{0}""" -f $languageId)
+	$removeEntriesRunLines += ("  echo ""Language {0}...""" -f $language.ToUpper())
+	$removeEntriesRunLines += ("  execute ""Remove-Language-{0}""" -f $language.ToUpper())
+	$removeEntriesRunLines += "ENDIF"
 	
-	$removeWhdloadsTempLines += ("IF EXISTS ""T:{0}""" -f $languageId)
-	$removeWhdloadsTempLines += ("  Delete >NIL: ""T:{0}""" -f $languageId)
-	$removeWhdloadsTempLines += "ENDIF"
+	$removeEntriesTempLines += ("IF EXISTS ""T:{0}""" -f $languageId)
+	$removeEntriesTempLines += ("  Delete >NIL: ""T:{0}""" -f $languageId)
+	$removeEntriesTempLines += "ENDIF"
 	
 	$languagesList = $languagesIndex.Get_Item($language)
 	
@@ -196,36 +196,36 @@ foreach($hardware in ($hardwaresIndex.Keys | Where-Object { $_ -notmatch '(ocs|e
 {
 	$hardwareId = CalculateMd5FromText $hardware.ToUpper()
 	
-	$removeWhdloadsMenuLines += ""
-	$removeWhdloadsMenuLines += ("; Hardware '{0}' menu" -f $hardware.ToUpper())
-	$removeWhdloadsMenuLines += ("echo ""Hardware {0} : "" NOLINE >>T:removewhdloadsmenu" -f $hardware.ToUpper())
-	$removeWhdloadsMenuLines += ("IF EXISTS ""T:{0}""" -f $hardwareId)
-	$removeWhdloadsMenuLines += "  echo ""YES"" >>T:removewhdloadsmenu"
-	$removeWhdloadsMenuLines += "ELSE"
-	$removeWhdloadsMenuLines += "  echo ""NO "" >>T:removewhdloadsmenu"
-	$removeWhdloadsMenuLines += "ENDIF"
+	$removeEntriesMenuLines += ""
+	$removeEntriesMenuLines += ("; Hardware '{0}' menu" -f $hardware.ToUpper())
+	$removeEntriesMenuLines += ("echo ""Hardware {0} : "" NOLINE >>T:removeentriesmenu" -f $hardware.ToUpper())
+	$removeEntriesMenuLines += ("IF EXISTS ""T:{0}""" -f $hardwareId)
+	$removeEntriesMenuLines += "  echo ""YES"" >>T:removeentriesmenu"
+	$removeEntriesMenuLines += "ELSE"
+	$removeEntriesMenuLines += "  echo ""NO "" >>T:removeentriesmenu"
+	$removeEntriesMenuLines += "ENDIF"
 
-	$removeWhdloadsMenuIndex++
-	$removeWhdloadsOptionLines += ""
-	$removeWhdloadsOptionLines += ("; Hardware '{0}' option" -f $hardware.ToUpper())
-	$removeWhdloadsOptionLines += ("IF ""`$removewhdloadsmenu"" eq ""{0}""" -f $removeWhdloadsMenuIndex)
-	$removeWhdloadsOptionLines += ("  IF EXISTS ""T:{0}""" -f $hardwareId)
-	$removeWhdloadsOptionLines += ("    Delete >NIL: ""T:{0}""" -f $hardwareId)
-	$removeWhdloadsOptionLines += "  ELSE"
-	$removeWhdloadsOptionLines += ("    echo """" NOLINE >""T:{0}""" -f $hardwareId)
-	$removeWhdloadsOptionLines += "  ENDIF"
-	$removeWhdloadsOptionLines += "ENDIF"
+	$removeEntriesMenuIndex++
+	$removeEntriesOptionLines += ""
+	$removeEntriesOptionLines += ("; Hardware '{0}' option" -f $hardware.ToUpper())
+	$removeEntriesOptionLines += ("IF ""`$removeentriesmenu"" eq ""{0}""" -f $removeEntriesMenuIndex)
+	$removeEntriesOptionLines += ("  IF EXISTS ""T:{0}""" -f $hardwareId)
+	$removeEntriesOptionLines += ("    Delete >NIL: ""T:{0}""" -f $hardwareId)
+	$removeEntriesOptionLines += "  ELSE"
+	$removeEntriesOptionLines += ("    echo """" NOLINE >""T:{0}""" -f $hardwareId)
+	$removeEntriesOptionLines += "  ENDIF"
+	$removeEntriesOptionLines += "ENDIF"
 
-	$removeWhdloadsRunLines += ""
-	$removeWhdloadsRunLines += ("; Remove hardware '{0}', if it's selected" -f $hardware.ToUpper())
-	$removeWhdloadsRunLines += ("IF EXISTS ""T:{0}""" -f $hardwareId)
-	$removeWhdloadsRunLines += ("  echo ""Hardware {0}...""" -f $hardware.ToUpper())
-	$removeWhdloadsRunLines += ("  execute ""Remove-Hardware-{0}""" -f $hardware.ToUpper())
-	$removeWhdloadsRunLines += "ENDIF"
+	$removeEntriesRunLines += ""
+	$removeEntriesRunLines += ("; Remove hardware '{0}', if it's selected" -f $hardware.ToUpper())
+	$removeEntriesRunLines += ("IF EXISTS ""T:{0}""" -f $hardwareId)
+	$removeEntriesRunLines += ("  echo ""Hardware {0}...""" -f $hardware.ToUpper())
+	$removeEntriesRunLines += ("  execute ""Remove-Hardware-{0}""" -f $hardware.ToUpper())
+	$removeEntriesRunLines += "ENDIF"
 
-	$removeWhdloadsTempLines += ("IF EXISTS ""T:{0}""" -f $hardwareId)
-	$removeWhdloadsTempLines += ("  Delete >NIL: ""T:{0}""" -f $hardwareId)
-	$removeWhdloadsTempLines += "ENDIF"
+	$removeEntriesTempLines += ("IF EXISTS ""T:{0}""" -f $hardwareId)
+	$removeEntriesTempLines += ("  Delete >NIL: ""T:{0}""" -f $hardwareId)
+	$removeEntriesTempLines += "ENDIF"
 	
 	$hardwaresList = $hardwaresIndex.Get_Item($hardware)
 	
@@ -245,72 +245,72 @@ foreach($hardware in ($hardwaresIndex.Keys | Where-Object { $_ -notmatch '(ocs|e
 }
 
 
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += '; Reset selected remove whdloads options'
-$removeWhdloadsLines += '; --------------------------------------'
-$removeWhdloadsLines += $removeWhdloadsTempLines
+$removeEntriesLines += ''
+$removeEntriesLines += ''
+$removeEntriesLines += '; Reset selected remove entries options'
+$removeEntriesLines += '; -------------------------------------'
+$removeEntriesLines += $removeEntriesTempLines
 
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += '; Build remove WHDLoads menu'
-$removeWhdloadsLines += '; --------------------------'
-$removeWhdloadsLines += 'LAB removewhdloadsmenu'
-$removeWhdloadsLines += "echo """" NOLINE >T:removewhdloadsmenu"
+$removeEntriesLines += ''
+$removeEntriesLines += ''
+$removeEntriesLines += '; Build remove entries menu'
+$removeEntriesLines += '; -------------------------'
+$removeEntriesLines += 'LAB removeentriesmenu'
+$removeEntriesLines += "echo """" NOLINE >T:removeentriesmenu"
 
-$removeWhdloadsLines += $removeWhdloadsMenuLines
+$removeEntriesLines += $removeEntriesMenuLines
 
-$removeWhdloadsLines += "echo ""=============================="" >>T:removewhdloadsmenu"
-$removeWhdloadsLines += "echo ""Remove selected WHDLoads"" >>T:removewhdloadsmenu"
-$removeWhdloadsLines += "echo ""Quit"" >>T:removewhdloadsmenu"
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += "; Show remove whdloads menu"
-$removeWhdloadsLines += "set removewhdloadsmenu """""
-$removeWhdloadsLines += "set removewhdloadsmenu ""``C:RequestList TITLE=""{0}"" LISTFILE=""T:removewhdloadsmenu"" WIDTH=320 LINES=20``""" -f $menuTitle
+$removeEntriesLines += "echo ""=============================="" >>T:removeentriesmenu"
+$removeEntriesLines += "echo ""Remove selected entries"" >>T:removeentriesmenu"
+$removeEntriesLines += "echo ""Quit"" >>T:removeentriesmenu"
+$removeEntriesLines += ''
+$removeEntriesLines += "; Show remove entries menu"
+$removeEntriesLines += "set removeentriesmenu """""
+$removeEntriesLines += "set removeentriesmenu ""``C:RequestList TITLE=""{0}"" LISTFILE=""T:removeentriesmenu"" WIDTH=320 LINES=20``""" -f $menuTitle
 
-$removeWhdloadsLines += "delete >NIL: T:removewhdloadsmenu"
+$removeEntriesLines += "delete >NIL: T:removeentriesmenu"
 
-$removeWhdloadsLines += $removeWhdloadsOptionLines
+$removeEntriesLines += $removeEntriesOptionLines
 
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += "; Remove selected whdloads option"
-$removeWhdloadsLines += ("IF ""`$removewhdloadsmenu"" eq """ + ($removeWhdloadsMenuIndex + 2) + """")
-$removeWhdloadsLines += "  set confirm ``RequestChoice ""Confirm"" ""Are you sure you want to remove selected WHDLoads?"" ""Yes|No""``"
-$removeWhdloadsLines += "  IF ""`$confirm"" EQ ""1"""
-$removeWhdloadsLines += "    SKIP runremovewhdloads"
-$removeWhdloadsLines += "  ENDIF"
-$removeWhdloadsLines += "ENDIF"
-$removeWhdloadsLines += ""
-$removeWhdloadsLines += "; Quit option"
-$removeWhdloadsLines += ("IF ""`$removewhdloadsmenu"" eq """ + ($removeWhdloadsMenuIndex + 3) + """")
-$removeWhdloadsLines += "  SKIP end"
-$removeWhdloadsLines += "ENDIF"
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += "SKIP BACK removewhdloadsmenu"
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += "; Remove WHDLoads"
-$removeWhdloadsLines += "; ---------------"
-$removeWhdloadsLines += "LAB runremovewhdloads"
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += "echo ""*e[1mRemoving WHDLoads...*e[0m"""
+$removeEntriesLines += ''
+$removeEntriesLines += "; Remove selected entries option"
+$removeEntriesLines += ("IF ""`$removeentriesmenu"" eq """ + ($removeEntriesMenuIndex + 2) + """")
+$removeEntriesLines += "  set confirm ``RequestChoice ""Confirm"" ""Are you sure you want to remove selected entries?"" ""Yes|No""``"
+$removeEntriesLines += "  IF ""`$confirm"" EQ ""1"""
+$removeEntriesLines += "    SKIP runremoveentries"
+$removeEntriesLines += "  ENDIF"
+$removeEntriesLines += "ENDIF"
+$removeEntriesLines += ""
+$removeEntriesLines += "; Quit option"
+$removeEntriesLines += ("IF ""`$removeentriesmenu"" eq """ + ($removeEntriesMenuIndex + 3) + """")
+$removeEntriesLines += "  SKIP end"
+$removeEntriesLines += "ENDIF"
+$removeEntriesLines += ''
+$removeEntriesLines += "SKIP BACK removeentriesmenu"
+$removeEntriesLines += ''
+$removeEntriesLines += ''
+$removeEntriesLines += "; Remove entries"
+$removeEntriesLines += "; ---------------"
+$removeEntriesLines += "LAB runremoveentries"
+$removeEntriesLines += ''
+$removeEntriesLines += "echo ""*e[1mRemoving entries...*e[0m"""
 
-$removeWhdloadsLines += $removeWhdloadsRunLines
+$removeEntriesLines += $removeEntriesRunLines
 
-$removeWhdloadsLines += "echo ""Done"""
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += "echo """""
-$removeWhdloadsLines += "echo ""Remove WHDLoads is complete."""
-$removeWhdloadsLines += "echo """""
-$removeWhdloadsLines += "ask ""Press ENTER to continue"""
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += "; End"
-$removeWhdloadsLines += "LAB end"
-$removeWhdloadsLines += ''
-$removeWhdloadsLines += '; Delete temp files, if they exist'
+$removeEntriesLines += "echo ""Done"""
+$removeEntriesLines += ''
+$removeEntriesLines += ''
+$removeEntriesLines += "echo """""
+$removeEntriesLines += "echo ""Remove entries is complete."""
+$removeEntriesLines += "echo """""
+$removeEntriesLines += "ask ""Press ENTER to continue"""
+$removeEntriesLines += ''
+$removeEntriesLines += "; End"
+$removeEntriesLines += "LAB end"
+$removeEntriesLines += ''
+$removeEntriesLines += '; Delete temp files, if they exist'
 
-$removeWhdloadsLines += $removeWhdloadsTempLines
+$removeEntriesLines += $removeEntriesTempLines
 
-$removeWhdloadsFile = Join-Path $outputDir -ChildPath 'Remove-WHDLoads'
-WriteAmigaTextLines $removeWhdloadsFile $removeWhdloadsLines
+$removeEntriesFile = Join-Path $outputDir -ChildPath 'Remove-Entries'
+WriteAmigaTextLines $removeEntriesFile $removeEntriesLines
